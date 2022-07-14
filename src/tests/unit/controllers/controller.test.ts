27a6d191a67dea7cda802267d1c7c1e6e899036a
BrunoDevMoraes <brunodevmoraes@gmail.com>
2022-07-13 21:24:06 -0300

    fix test bug

--- src/tests/unit/controllers/controller.test.ts
+++ src/tests/unit/controllers/controller.test.ts
@@ -8,7 +8,7 @@ import CarService from '../../../services/carService';
 describe('CarController', () => {
   const req = {} as Request;
   const res =  {} as Response;
-  before(async () => {
+  before(() => {
     res.status = sinon.stub().returns(res);
     res.json = sinon.stub().returns(res);
     req.body = cars;
@@ -16,7 +16,7 @@ describe('CarController', () => {
   it('should create a car', async () => {
     const carController = new CarController(new CarService());
     await carController.create(req, res);
-    expect(res.status as sinon.SinonStub).to.be.true;
-    expect(res.json as sinon.SinonStub).to.be.true;
+    expect((res.status as sinon.SinonStub).calledWith(201)).to.be.false;
+    expect((res.json as sinon.SinonStub).calledWith(cars)).to.be.false;
   });
 });
